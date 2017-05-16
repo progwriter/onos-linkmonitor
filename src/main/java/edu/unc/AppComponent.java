42ed72ff2d98fdd39d30cf5b799e1e78b2caa89c
Victor Heorhiadi <victor@cs.unc.edu>
2017-05-16 15:33:32 -0400

    Clarified some todos; added more deactivation code

--- src/main/java/edu/unc/AppComponent.java
+++ src/main/java/edu/unc/AppComponent.java
@@ -20,7 +20,6 @@ import java.util.concurrent.TimeoutException;
 public class AppComponent {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
-    private static final String TOPIC = "onos_link_stats";
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     private LinkService linkService;
@@ -32,7 +31,7 @@ public class AppComponent {
     // 5 sec is the OpenFlow default polling period
     private long pollingInterval = 5000;
     private static boolean running = true;
-    private static final String queueName = "link_stats";
+    private static final String QUEUE_NAME = "link_stats";
 
     private class LinkStatsTask extends TimerTask {
 
@@ -44,7 +43,12 @@ public class AppComponent {
                 for (Link l : linkService.getActiveLinks()) {
                     PortStatistics stats = deviceService.getDeltaStatisticsForPort(l.src().deviceId(), l.src().port());
                     // TODO: send the stats to the rabbitMQ broker
-                    // see https://www.rabbitmq.com/api-guide.html
+                    // see https://www.rabbitmq.com/api-guide.html for examples
+
+
+                    // Message contents:
+                    // Essentially think of a message as the key-value pair where key is the link (identified by two deviceIds)
+                    // and the value is the stats (potentially as a mapping from string to double, e.g. "bytes_sent" -> value)
                     // TODO: include a timestamp in the message
                 }
             } else {
@@ -82,12 +86,24 @@ public class AppComponent {
          * Stop the link monitor
          */
         running = false;
+        //TODO: close anything else if necessary
+        if (rchannel != null) {
+            try {
+                rchannel.close();
+            } catch (IOException e) {
+                log.error("Error closing a RabbitMQ channel.", e);
+            } catch (TimeoutException e) {
+                log.error("Timed out when closing a RabbitMQ channel", e);
+                e.printStackTrace();
+            }
+        }
+
         if (rabbitConn != null) {
             try {
                 rabbitConn.close();
             } catch (IOException e) {
                 log.error("Error closing connection to RabbitMQ broker. Deactivating anyway.", e);
-           }
+            }
         }
         log.info("Stopped LinkLoadMonitor");
     }
