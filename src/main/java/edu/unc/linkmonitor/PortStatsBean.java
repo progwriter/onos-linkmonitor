07e9a100a9a2ef5a9aae63685e76e4c20b1e1767
Victor Heorhiadi <victor@cs.unc.edu>
2017-09-29 14:01:53 -0400

    Formatting; switched to StringJoiner for more efficient string building.

--- src/main/java/edu/unc/linkmonitor/PortStatsBean.java
+++ src/main/java/edu/unc/linkmonitor/PortStatsBean.java
@@ -1,17 +1,19 @@
 package edu.unc.linkmonitor;
 
-import org.onosproject.net.device.PortStatistics;
 import org.onosproject.net.DeviceId;
+import org.onosproject.net.Link;
 import org.onosproject.net.PortNumber;
 import org.onosproject.net.device.PortStatistics;
-import java.net.URI;
-import java.io.Writer;
-import java.io.IOException;
-import org.onosproject.net.Link;
-import java.time.LocalTime;
-import java.lang.Long;
 
-/** The object model for the data we are sending through endpoints */
+import java.io.IOException;
+import java.io.Writer;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.StringJoiner;
+
+/**
+ * The object model for the data we are sending through endpoints
+ */
 public class PortStatsBean {
 
     String DeviceId;
@@ -26,113 +28,131 @@ public class PortStatsBean {
     String TxPacketsDropped;
     String TxPacketErrors;
 
-    //TODO: write constructor
 
     public PortStatsBean(Link link, PortStatistics stats) {
-	this.DeviceId = link.src().deviceId().uri().toString();
-	this.SrcPort = String.valueOf(link.src().port().toLong());
-	this.DstDeviceId = link.dst().deviceId().uri().toString();
-	this.DstPort = String.valueOf(link.dst().port().toLong());
-	this.Age = Long.toString(stats.durationSec());
-	this.PacketsReceived = Long.toString(stats.packetsReceived());
-	this.RxPacketsDropped = Long.toString(stats.packetsRxDropped());
-	this.RxPacketErrors = Long.toString(stats.packetsRxErrors());
-	this.PacketsSent = Long.toString(stats.packetsSent());
-	this.TxPacketsDropped = Long.toString(stats.packetsTxDropped());
-	this.TxPacketErrors = Long.toString(stats.packetsTxErrors());
+        this.DeviceId = link.src().deviceId().uri().toString();
+        this.SrcPort = String.valueOf(link.src().port().toLong());
+        this.DstDeviceId = link.dst().deviceId().uri().toString();
+        this.DstPort = String.valueOf(link.dst().port().toLong());
+        this.Age = Long.toString(stats.durationSec());
+        this.PacketsReceived = Long.toString(stats.packetsReceived());
+        this.RxPacketsDropped = Long.toString(stats.packetsRxDropped());
+        this.RxPacketErrors = Long.toString(stats.packetsRxErrors());
+        this.PacketsSent = Long.toString(stats.packetsSent());
+        this.TxPacketsDropped = Long.toString(stats.packetsTxDropped());
+        this.TxPacketErrors = Long.toString(stats.packetsTxErrors());
     }
 
     public String getDeviceId() {
-	return this.DeviceId;
+        return this.DeviceId;
     }
+
     public void setDeviceId(DeviceId deviceId) {
-	this.DeviceId = deviceId.toString();
+        this.DeviceId = deviceId.toString();
     }
 
     public String getSrcPort() {
-	return this.SrcPort;
+        return this.SrcPort;
     }
+
     public void setSrcPort(PortNumber port) {
-	this.SrcPort = port.toString();
+        this.SrcPort = port.toString();
     }
 
     public String getDstDeviceId() {
-	return this.DstDeviceId;
+        return this.DstDeviceId;
     }
-    
+
     public void setDstDeviceId(DeviceId deviceId) {
-	this.DstDeviceId = deviceId.toString();
-    }    
+        this.DstDeviceId = deviceId.toString();
+    }
 
     public String getDstPort() {
-	return this.DstPort;
+        return this.DstPort;
     }
+
     public void setDstPort(PortNumber port) {
-	this.DstPort = port.toString();
+        this.DstPort = port.toString();
     }
-    
+
     public String getAge() {
-	return this.Age;
+        return this.Age;
     }
+
     public void setAge(double age) {
-	this.Age = String.valueOf(age);
+        this.Age = String.valueOf(age);
     }
 
     public String getPacketsReceived() {
-	return this.PacketsReceived;
+        return this.PacketsReceived;
     }
+
     public void setPacketsReceived(long packetsReceived) {
-	this.PacketsReceived = String.valueOf(packetsReceived);
+        this.PacketsReceived = String.valueOf(packetsReceived);
     }
+
     public String getPacketsSent() {
-	return this.PacketsSent;
+        return this.PacketsSent;
     }
+
     public void setPacketsSent(long packetsSent) {
-	this.PacketsSent = String.valueOf(packetsSent);
+        this.PacketsSent = String.valueOf(packetsSent);
     }
-    
- 
+
+
     public String getRxPacketsDropped() {
-	return this.RxPacketsDropped;
+        return this.RxPacketsDropped;
     }
+
     public void setRxPacketsDropped(long packetsDropped) {
-	this.RxPacketsDropped = String.valueOf(packetsDropped);
+        this.RxPacketsDropped = String.valueOf(packetsDropped);
     }
+
     public String getRxPacketErrors() {
-	return this.RxPacketErrors;
+        return this.RxPacketErrors;
     }
+
     public void setRxPacketErrors(long packetErrors) {
-	this.RxPacketErrors = String.valueOf(packetErrors);
+        this.RxPacketErrors = String.valueOf(packetErrors);
     }
 
 
     public String getTxPacketsDropped() {
-	return this.TxPacketsDropped;
+        return this.TxPacketsDropped;
     }
+
     public void setTxPacketsDropped(long packetsDropped) {
-	this.TxPacketsDropped = String.valueOf(packetsDropped);
+        this.TxPacketsDropped = String.valueOf(packetsDropped);
     }
+
     public String getTxPacketErrors() {
-	return this.TxPacketErrors;
+        return this.TxPacketErrors;
     }
+
     public void setTxPacketErrors(long packetErrors) {
-	this.TxPacketErrors = String.valueOf(packetErrors);
+        this.TxPacketErrors = String.valueOf(packetErrors);
     }
 
     public static String getHeaders() {
-	String entry = "Time,DeviceId,SrcPort,DstDeviceId,DstPort,Age,PktsRecvd,PktsSent,RxPktDrop,TxPktDrop,RxPktErr,TxPktErn\n";
-	return entry;
+        String entry = "Time,DeviceId,SrcPort,DstDeviceId,DstPort,Age,PktsRecvd,PktsSent,RxPktDrop,TxPktDrop,RxPktErr,TxPktErn\n";
+        return entry;
     }
 
-    public boolean write(Writer file) {
-	try {
-	    String entry = LocalTime.now().toString() + "," + getDeviceId() + "," + getSrcPort() + "," + getDstDeviceId() + "," + getDstPort() + "," + getAge() + "," + getPacketsReceived() + "," + getPacketsSent() + ","  + getRxPacketsDropped() + "," + getTxPacketsDropped() + "," + getRxPacketErrors() + "," + getTxPacketErrors() + "\n";
-	    file.write(entry);
-	    file.flush();
-	    return true;
-	} catch (IOException e) {
-	    return false;
-	}
+    public void write(Writer file) throws IOException {
+        StringJoiner joiner = new StringJoiner(",");
+        joiner.add(ZonedDateTime.now(ZoneOffset.UTC).toString());
+        joiner.add(getDeviceId());
+        joiner.add(getSrcPort());
+        joiner.add(getDstDeviceId());
+        joiner.add(getDstPort());
+        joiner.add(getAge());
+        joiner.add(getPacketsReceived());
+        joiner.add(getPacketsSent());
+        joiner.add(getRxPacketsDropped());
+        joiner.add(getTxPacketsDropped());
+        joiner.add(getRxPacketErrors());
+        joiner.add(getTxPacketErrors());
+        file.write(joiner.toString() + "\n");
+        file.flush();
     }
-
 }
